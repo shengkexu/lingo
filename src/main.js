1ce3567aca48a552abc05eb857d2db10f4f7902b
shengkexu <xushengke94@gmail.com>
2020-09-08 15:06:43 +0800

    first push

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//import VueRouter from 'vue-router'
+import './style/reset.css'
+import './style/main.css'
+
+createApp(App).mount('#app')
---
+++ src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <Menu/>
+    <div class="word">
+        <p>Lively  Innovative  Nactive  Go  Outbraving</p>
+    </div>
+</template>
+
+<script>
+import Menu from '../components/Menu.vue'
+export default {
+    name: 'Home',
+    components: {
+    Menu,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
