3c62bc9e651aeeffadf56b77444fc0afff9c7b8f
shengkexu <xushengke94@gmail.com>
2020-09-11 16:01:13 +0800

    menu animation

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,22 +1,27 @@
 <template>
-    <nav id="mainNav">
+    <!-- <nav id="mainNav"> -->
       <div id="menuToggle" v-on:click="toggleMenu">
           <span></span>
           <span></span>
           <span></span>
       </div>
       <div class="menuMain">
-        <img id="triangleWhite" src="../assets/triangleWhite1.png" alt="triangleWhite1">
-        <img id="triangleBlack" src="../assets/triangleBlack1.png" alt="triangleBlack1">
+        <img class="menuBack" id="triangleWhite" :class="{'moveWhite': !isActive}" src="../assets/triangleWhite1.png" alt="triangleWhite1">
+        <img class="menuBack" id="triangleBlack" :class="{'moveBlack': !isActive}" src="../assets/triangleBlack1.png" alt="triangleBlack1">
+        <img class="logo" src="../assets/lingoo.png" alt="lingoo">
         <ul id="menu">
-            <a href="#"><li>Home</li></a>
-            <a href="#"><li>About</li></a>
-            <a href="#"><li>Info</li></a>
-            <a href="#"><li>Contact</li></a>
-            <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+            <a href="#"><li>首页</li></a>
+            <a href="#"><li>案例展示</li></a>
+            <a href="#"><li>关于我们</li></a>
+            <a href="#"><li>联系我们</li></a>
         </ul>
+        <div class="menuInfo">
+          <div class="">
+            <img src="../assets/addressIcon.png" alt="">
+          </div>
+        </div>
       </div>
-    </nav>
+    <!-- </nav> -->
 
 </template>
 <script>
@@ -43,5 +48,12 @@ export default {
 }
 </script>
 <style scoped>
-
+.moveWhite{
+  left: 400px !important;
+  transition: 1s all cubic-bezier(0.77,0.2,0.05,1.0);
+}
+.moveBlack{
+  left: -300px !important;
+  transition: 1s all cubic-bezier(0.77,0.2,0.05,1.0);
+}
 </style>
