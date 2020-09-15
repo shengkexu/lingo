e745e40ec9d63796de65f65699fc1366b955a9ff
shengkexu <xushengke94@gmail.com>
2020-09-15 15:32:02 +0800

    menu almost done

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,27 +1,22 @@
 <template>
     <!-- <nav id="mainNav"> -->
       <div id="menuToggle" v-on:click="toggleMenu">
-          <span></span>
-          <span></span>
-          <span></span>
+          <span :class="{'ham1': isActive}"></span>
+          <span :class="{'ham2': isActive}"></span>
+          <span :class="{'ham3': isActive}"></span>
       </div>
-      <!-- <div class="menuMain">
-        <img class="menuBack" id="triangleWhite" :class="{'moveWhite': !isActive}" src="../assets/triangleWhite1.png" alt="triangleWhite1">
-        <img class="menuBack" id="triangleBlack" :class="{'moveBlack': !isActive}" src="../assets/triangleBlack1.png" alt="triangleBlack1">
-        <img class="logo" src="../assets/lingoo.png" alt="lingoo">
-        <ul id="menu">
-            <a href="#"><li>首页</li></a>
-            <a href="#"><li>案例展示</li></a>
-            <a href="#"><li>关于我们</li></a>
-            <a href="#"><li>联系我们</li></a>
+      <div class="menuCon">
+        <div class="blueCircle" ref="circle1" :class="{'blueCircleScale': isActive}"></div>
+        <div class="yellowCircle" ref="circle2" :class="{'yellowCircleScale': isActive}"></div>
+        <div class="redCircle" ref="circle3" :class="{'redCircleScale': isActive}"></div>
+        <ul class="menu" ref="menuWordCon">
+            <a href="#"><li class="menuWord menuWord1" :class="{'menuWordChange1': isActive}">首页</li></a>
+            <a href="#"><li class="menuWord menuWord2" :class="{'menuWordChange2': isActive}">案例展示</li></a>
+            <a href="#"><li class="menuWord menuWord3" :class="{'menuWordChange3': isActive}">关于我们</li></a>
+            <a href="#"><li class="menuWord menuWord4" :class="{'menuWordChange4': isActive}">联系我们</li></a>
         </ul>
-        <div class="menuInfo">
-          <div class="">
-            <img src="../assets/addressIcon.png" alt="">
-          </div>
-        </div>
-      </div> -->
-    <!-- </nav> -->
+
+      </div>
 
 </template>
 <script>
@@ -35,25 +30,41 @@ export default {
     methods:{
       toggleMenu: function (){
         if(this.isActive == false){
+          this.$refs.circle1.style.transition = '0.8s all cubic-bezier(.85,0,.21,1)'
+          this.$refs.circle2.style.transition = '1s all cubic-bezier(.85,0,.21,1)'
+          this.$refs.circle3.style.transition = '1.2s all cubic-bezier(.85,0,.21,1)'
+          this.$refs.menuWordCon.style.top = '0'
+          // this.$refs.menuWord.style.display = 'block'
+          // this.$refs.menuWord.style.opacity = '1'
           this.isActive = true
+
         }else{
+          this.$refs.circle1.style.transition = '1.2s all cubic-bezier(.85,0,.21,1)'
+          this.$refs.circle2.style.transition = '1s all cubic-bezier(.85,0,.21,1)'
+          this.$refs.circle3.style.transition = '0.8s all cubic-bezier(.85,0,.21,1)'
+          this.$refs.menuWordCon.style.top = '-100vh'
+          // this.$refs.menuWord.style.display = 'none'
+          // this.$refs.menuWord.style.opacity = '0'
           this.isActive = false
         }
         console.log(this.isActive)
       }
     },
     mounted(){
-      console.log(this.isActive)
+      this.isActive = false
+      console.log(this.$refs)
     },
 }
 </script>
-<style scoped>
-.moveWhite{
+<style>
+/* .moveWhite{
   left: 400px !important;
   transition: 1s all cubic-bezier(0.77,0.2,0.05,1.0);
 }
 .moveBlack{
   left: -300px !important;
   transition: 1s all cubic-bezier(0.77,0.2,0.05,1.0);
-}
+} */
+
+
 </style>
